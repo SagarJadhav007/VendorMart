3766067334160507ca7de084fb13cff4f1983794
Sagar Jadhav <jadhavsagar1505@gmail.com>
2025-07-26 22:35:59 +0530

    a

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use("/api/auth", authRoutes);
+app.use("/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
